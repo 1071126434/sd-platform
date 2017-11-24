7ef78e8893e010b2245a6da14ce7e07cee713169
yanglx <1071126434@qq.com>
2017-11-24 17:45:23 +0800

    port

--- config/index.js
+++ config/index.js
@@ -26,7 +26,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: process.env.PORT || 3335,
+    port: process.env.PORT || 8887,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
