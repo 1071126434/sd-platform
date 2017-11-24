43ddaefefa97954f33644315779995bc1a0a8493
yanglx <1071126434@qq.com>
2017-11-24 17:59:21 +0800

    启动js

---
+++ sell.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var proxy = require('http-proxy-middleware')
+var history = require('connect-history-api-fallback')
+  // var config = require('./config/index')
+var compression = require('compression')
+var app = express()
+app.use(compression())
+app.use('/api', proxy({
+  target: 'http://182.61.29.51:8089',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': ''
+  }
+}))
+app.use(history({
+  index: './index.html'
+}))
+app.use(express.static('./dist'))
+  // var port = process.env.PORT || config.build.port
+var port = 8887
+module.exports = app.listen(port, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('Listening at http://localhost:' + port + '\n')
+})
