96b548f0548ce039b653aa89dc6d7e7581586e7f
yanglx <1071126434@qq.com>
2017-11-24 20:59:54 +0800

    ..config

--- src/assets/js/http.js
+++ src/assets/js/http.js
@@ -22,7 +22,7 @@ axios.interceptors.request.use((config) => {
 axios.interceptors.response.use((res) => {
   // 需做用户token和userid存储
   if (parseInt(res.status) === 200) {
-    setUserTokenStorage(res.headers.accesstoken)
+    setUserTokenStorage(res.config.headers.accesstoken)
   }
   return res
 }, (error) => {
