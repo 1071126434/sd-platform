4c42ea61a33917d21fdd448e386d39bfb7be8418
yanglx <1071126434@qq.com>
2017-11-29 13:09:43 +0800

    兼容处理

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import axios from './assets/js/http'
+import 'babel-polyfill'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/stylus/index.styl'
 
